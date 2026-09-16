74ccd7ea41bf67041c3d8f57ae9096a33744483c
LuisRenartDam1 <lrenart2005@gmail.com>
2026-09-16 16:26:41 +0200

    Implent Player.java

---
+++ src/worldcup/Player.java
@@ -0,0 +1,17 @@
+package worldcup;
+
+public class Player extends Person {
+	private String position;
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public void passBall() {
+		System.out.println("passing the ball");
+	}
+}
