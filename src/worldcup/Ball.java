92cfa07fcd347dd4c15d1e83bc92f87159a6ab5e
Daniel Labrada <labrada22dani@gmail.com>
2026-09-17 15:36:58 +0200

    Add Ball class with material property and movement method

---
+++ src/worldcup/Ball.java
@@ -0,0 +1,18 @@
+package worldcup;
+public class Ball {
+
+	private String material;
+
+	public void move() {
+			System.out.println("moving the ball");
+
+	}
+	public String getMaterial() {
+		return material;
+	}
+
+		public void setMaterial(String material) {
+		this.material = material;
+	}
+
+}
